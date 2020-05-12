b6fbc6a2c8f306012d382bf89fe4ad9705224fdb
Murilo Pupo de Oliveira <48927673+mpupo@users.noreply.github.com>
2020-05-12 20:01:36 -0300

    Pilha funcionando

---
+++ Pilha/ConsoleApplication1/main.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include "pilha.h"
+
+// Aluno: Murilo Pupo de Oliveira ADS 3 Noite
+
+
+int main()
+{
+
+    Stack<std::string> pilha = Stack<std::string>(50);
+    std::string frase;
+    int opcao;
+    do
+    {
+        std::cout << "Palindromos:" << std::endl;
+        std::cout << "1 - Inserir nova frase" << std::endl;
+        std::cout << "9 - Fim" << std::endl;
+        std::cout << "Selecione:";
+        std::cin >> opcao;
+        switch (opcao)
+        {
+        case 1:
+            std::cout << "Digite a frase:";
+            std::cin.clear();
+            std::cin.ignore();
+            //std::cin >> frase;
+            std::getline(std::cin, frase);
+
+            std::cout << "A palavra digitada foi: " << frase << std::endl;
+
+            if (pilha.sao_palindromos(frase))
+            {
+                std::cout << "A palavra " << frase << " eh palindroma!" << std::endl;
+            }
+            else
+            {
+                std::cout << "A palavra " << frase << " nao eh pali'ndroma!" << std::endl;
+            }
+
+            pilha.clear();
+            system("pause");
+            system("cls");
+            break;
+        case 9:
+        {
+            std::cout << "Fim..." << std::endl;
+            system("pause");
+            system("cls");
+            break;
+        }
+        default:
+        {
+            std::cout << "opcao invalida" << std::endl;
+            system("pause");
+            system("cls");
+            break;
+        }
+        }
+    } while (opcao != 9);
+
+    return 0;
+}
---
+++ Pilha/ConsoleApplication1/pilha.h
@@ -0,0 +1,150 @@
+//#ifndef PILHA_H_INCLUDED
+//#define PILHA_H_INCLUDED
+
+#pragma once
+#include <string>
+#include <vector>
+#include <iostream>
+// Aluno: Murilo Pupo de Oliveira ADS 3 Noite
+
+template <typename Generic>
+struct Stack
+{
+    Generic *stack;
+    int top;
+    int size;
+
+    Stack(int tam)
+    {
+        size = tam;
+        top = -1;
+        stack = new Generic[size];
+    }
+
+    ~Stack()
+    {
+        delete stack;
+    }
+
+    void push(char x)
+    {
+
+        top++;
+        stack[top] = x;
+    }
+
+    Generic pop()
+    {
+        Generic temp = stack[top];
+        top--;
+        return temp;
+    }
+
+    void clear()
+    {
+        top = -1;
+    }
+
+    Generic firstElement()
+    {
+        return stack[top];
+    }
+
+    bool is_full()
+    {
+        return top == size - 1;
+    }
+
+    bool is_empty()
+    {
+        return top == -1;
+    }
+
+    void showElements()
+    {
+        for (int i = top; i >= 0; i--)
+        {
+            std::cout << stack[i] << std::endl;
+        }
+    }
+
+    void insertPhrase(std::string phrase)
+    {
+        for (int letter = 0; letter < phrase.size(); letter++)
+        {
+            push(phrase.at(letter));
+        };
+    }
+
+    std::string removeCapitalLetter(std::string input)
+    {
+        std::string output = "";
+        for (int letter = 0; letter < input.size(); letter++)
+        {
+            output += char(tolower(input.at(letter)));
+        }
+        return output;
+    }
+
+    std::string removePunctuation(std::string input)
+    {
+        std::vector<char> punctuations = {'!', ';', ',', '.', '#', '?', '-', '_'};
+
+        for (int letter = 0; letter < input.size(); letter++)
+        {
+            for (int pont = 0; pont < punctuations.size(); pont++)
+            {
+                if (input[letter] == punctuations[pont])
+                {
+                    input.at(letter) = ' '; // '' pois é um unico elemento de char
+                }
+            }
+        }
+        return input;
+    }
+
+    std::string removeBlankSpaces(std::string input)
+    {
+        std::string output = ""; // "" pois é uma array de string que vai ser concatenada
+
+        for (int letter = 0; letter < input.size(); letter++)
+        {
+            if (input.at(letter) != ' ')
+            {
+                output += input.at(letter);
+            }
+        }
+        return output;
+    }
+
+    bool sao_palindromos(std::string input)
+    {
+        input = removePunctuation(input);
+        input = removeBlankSpaces(input);
+        input = removeCapitalLetter(input);
+        insertPhrase(input);
+
+        std::string letter;
+        std::string stack_letter;
+
+
+        bool isPalindromo = true;
+        for (int i = 0; i < input.length(); i++)
+        {
+            letter = input[i];
+            stack_letter = firstElement();
+            if (letter == stack_letter)
+            {
+                pop();
+            }
+            else
+            {
+                isPalindromo = false;
+                break;
+            }
+        }
+        return isPalindromo;
+    };
+};
+
+//#endif PILHA_H_INCLUDED
